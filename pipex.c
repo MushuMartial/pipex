436843c396fb8bbc95ddc1031898b700ca05a2e9
Mushu <92597474+MushuMartial@users.noreply.github.com>
2021-12-15 18:17:26 +0100

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -6,24 +6,22 @@
 /*   By: tmartial <tmartial@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 11:25:10 by tmartial          #+#    #+#             */
-/*   Updated: 2021/12/15 15:58:02 by tmartial         ###   ########.fr       */
+/*   Updated: 2021/12/15 17:43:07 by tmartial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-//path possibles PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki
+//path PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki
 //env[6] = path
-#define STDIN  0
-#define STDOUT  1
-int main(int argc, char *argv[], char *env[])
+
+int	main(int argc, char *argv[], char *env[])
 {
-	int fdin;
-	int fdout;
-	char **cmd1;
-	char **cmd2;
-	char *path_name;
-	t_paths all_paths;
-	
+	int		file1;
+	int		file2;
+	char	**cmd1;
+	char	*path_name;
+	t_paths	all_paths;
+
 	if (argc == 5)
 	{
 		cmd1 = malloc(sizeof(char) * 2);
@@ -32,16 +30,13 @@ int main(int argc, char *argv[], char *env[])
 		cmd1[1] = 0;
 		cmd2[0] = ft_strdup(argv[3]);
 		cmd2[1] = 0;
-	
 		path_init(&all_paths, cmd1[0]);
 		path_name = find_path(cmd1[0], all_paths);
-		fdin = open(argv[1], O_RDONLY);
-		fdout = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-		
-		dup2(fdin, STDIN);
-		dup2(fdout, STDOUT);
-		redir(cmd1, env, fdin, path_name);
-		
+		file1 = open(argv[1], O_RDONLY);
+		file2 = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		dup2(file1, 0);
+		dup2(file2, 1);
+		redir(cmd1, env, file1, path_name);
 		path_init(&all_paths, cmd2[0]);
 		path_name = find_path(cmd2[0], all_paths);
 		if (execve(path_name, cmd2, env) == -1)
@@ -49,11 +44,10 @@ int main(int argc, char *argv[], char *env[])
 	}
 	else
 		perror("Invalid args");
-	return(0);
-	
+	return (0);
 }
 
-void	redir (char **cmd, char **env, int fdin, char *path_name)
+void	redir(char **cmd, char **env, int file1, char *path_name)
 {
 	pid_t	pid;
 	int		pipefd[2];
@@ -63,14 +57,13 @@ void	redir (char **cmd, char **env, int fdin, char *path_name)
 	if (pid)
 	{
 		close(pipefd[1]);
-		dup2(pipefd[0], STDIN);
-		// waitpid(pid, NULL, 0);
+		dup2(pipefd[0], 0);
 	}
 	else
 	{
 		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT);
-		if (fdin == STDIN)
+		dup2(pipefd[1], 1);
+		if (file1 == 0)
 			exit(1);
 		else
 		{
--- pipex.h
+++ pipex.h
@@ -6,28 +6,28 @@
 /*   By: tmartial <tmartial@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 13:59:29 by tmartial          #+#    #+#             */
-/*   Updated: 2021/12/15 15:40:14 by tmartial         ###   ########.fr       */
+/*   Updated: 2021/12/15 17:28:45 by tmartial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
 typedef struct s_paths {
-	char *path1;
-	char *path2; 
-	char *path3;
-	char *path4; 
-	char *path5; 
-	char *path6;
+	char	*path1;
+	char	*path2;
+	char	*path3;
+	char	*path4;
+	char	*path5;
+	char	*path6;
 }	t_paths;
 
 /* libft */
@@ -35,12 +35,11 @@ char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 
-
 /* main */
-void	redir (char **cmd, char **env, int fdin, char *path_name);
+void	redir(char **cmd, char **env, int fdin, char *path_name);
 
 /* utils */
-void path_init(t_paths *path, char *cmd);
-char *find_path(char *cmd, t_paths path);
+void	path_init(t_paths *path, char *cmd);
+char	*find_path(char *cmd, t_paths path);
 
 #endif
